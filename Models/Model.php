8a836c41b8fcbdbdfd48f116919c25e887dc0364
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-13 19:42:25 +0100

    maj sans commentaires inutile

--- Models/Model.php
+++ Models/Model.php
@@ -4,15 +4,6 @@ namespace App\Models;
 
 class Model
 {
-    /**
-     * Hydrates the current object with data.
-     *
-     * Takes an associative array of data and uses setter methods to populate
-     * the object's properties.
-     *
-     * @param object $data The data to hydrate the object with.
-     * @return self Returns the current instance of the object for method chaining.
-     */
     public function hydrate($data)
     {
         foreach ($data as $key => $value) {
