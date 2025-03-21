131c030f28c9ab4df7d0b0e81074f61f75c5da2d
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-21 19:31:58 +0100

    add du formulaire d ajout user

---
+++ Views/Dashboard/AjoutUser.php
@@ -0,0 +1,37 @@
+<?php
+$css = "user";
+?>
+
+<section class="text-center d-flex justify-content-center align-items-center racing-form-section">
+    <div class="container p-4 col-12 col-md-8 col-lg-4 racing-form-container">
+        <h2 class="racing-title"><i class="fas fa-user-plus"></i> Inscription </h2>
+        <form method="POST" class="p-3 racing-form" id="loginForm" action="/DashUser/AjoutUser">
+            <div class="form-group">
+                <label for="name"><i class="fas fa-user"></i> Pseudo :</label>
+                <input type="text" name="name" id="name" class="form-control mb-3 racing-input" required>
+            </div>
+            <div class="form-group">
+                <label for="email"><i class="fas fa-envelope"></i> Email :</label>
+                <input type="email" name="email" id="email" class="form-control mb-3 racing-input" required>
+            </div>
+            <div class="form-group">
+                <label for="password"><i class="fas fa-lock"></i> Mot de passe :</label>
+                <input type="password" name="password" id="password" class="form-control mb-3 racing-input" required>
+            </div>
+            <div class="form-group">
+                <label for="confirmPassword"><i class="fas fa-lock"></i> Confirmation du mot de passe :</label>
+                <input type="password" name="confirmPassword" id="confirmPassword" class="form-control mb-3 racing-input" required>
+            </div>
+            <div class="formLogin">
+                <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+                <button type="submit" id="submit" class="btn racing-btn w-75"> S'inscrire</button>
+            </div>
+        </form>
+
+        <!-- Messages d'alerte -->
+        <div>
+            <div id="error-message" class="alert alert-danger d-none" role="alert"></div>
+            <div id="success-message" class="alert alert-success d-none" role="alert"></div>
+        </div>
+    </div>
+</section>
