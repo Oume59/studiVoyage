43d115b113510049cc41eb3b345ea1ad06c8e180
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-26 19:03:49 +0100

    add view user des differents choix de croisieres

---
+++ Views/Croisieres/index.php
@@ -0,0 +1,14 @@
+<?php
+$css='croisiere';
+?>
+
+<section class="croisiere-choice-full">
+    <div class="croisiere-inner text-center">
+        <h1 class="py-4">🛳️ Découvrez nos croisières</h1>
+        <div class="d-flex justify-content-center gap-4 flex-wrap mt-4">
+            <a href="/CroisiereM" class="btn btn-outline-primary px-4 py-2">Croisière Méditerranée</a>
+            <a href="/CroisiereC" class="btn btn-outline-primary px-4 py-2">Croisière Caraïbes</a>
+            <a href="/CroisiereEx" class="btn btn-outline-primary px-4 py-2">Expédition Polaire</a>
+        </div>
+    </div>
+</section>
