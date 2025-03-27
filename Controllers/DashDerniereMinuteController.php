cf67cb4a25e76c81d82b3709e528af88ab654ebe
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-27 22:54:27 +0100

    maj du controller dash de l admin derniere minute

--- Controllers/DashDerniereMinuteController.php
+++ Controllers/DashDerniereMinuteController.php
@@ -6,11 +6,11 @@ use App\Services\DerniereMinuteService;
 
 class DashDerniereMinuteController extends Controller
 {
-    private $service;
+    private $derniereMinuteService;
 
     public function __construct()
     {
-        $this->service = new DerniereMinuteService();
+        $this->derniereMinuteService = new DerniereMinuteService();
     }
 
     public function index()
@@ -21,29 +21,29 @@ class DashDerniereMinuteController extends Controller
     public function ajoutDerniereMinute()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $result = $this->service->addOffre($_POST, $_FILES);
+            $result = $this->derniereMinuteService->addOffre($_POST, $_FILES);
             echo json_encode($result);
             exit();
         }
-        header("Location: /Dashboard/index");
+        header("Location: /DashDerniereMinute/liste");
         exit();
     }
 
     public function updateDerniereMinute($id)
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $result = $this->service->updateOffre($id, $_POST, $_FILES);
+            $result = $this->derniereMinuteService->updateOffre($id, $_POST, $_FILES);
             echo json_encode($result);
             exit();
         }
-        $offre = $this->service->findOffreById($id);
+        $offre = $this->derniereMinuteService->findOffreById($id);
         $this->render("Dashboard/UpdateDerniereMinute", ['offre' => $offre]);
     }
 
     public function deleteDerniereMinute()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['id'])) {
-            $result = $this->service->deleteOffre($_POST['id']);
+            $result = $this->derniereMinuteService->deleteOffre($_POST['id']);
             echo json_encode($result);
             exit();
         }
@@ -53,7 +53,7 @@ class DashDerniereMinuteController extends Controller
 
     public function liste()
     {
-        $offres = $this->service->getAllOffres();
+        $offres = $this->derniereMinuteService->getAllOffres();
         $this->render("Dashboard/ListeDerniereMinute", ['offres' => $offres]);
     }
 }
