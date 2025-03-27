bc16abd30363917bebfede58b023f7ec192b2e16
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-03-27 22:51:56 +0100

    créa de la liste  dash  dernieres minutes

--- Views/Dashboard/ListeDerniereMinute.php
+++ Views/Dashboard/ListeDerniereMinute.php
@@ -18,19 +18,19 @@
             <tbody>
                 <?php foreach ($offres as $offre): ?>
                     <tr>
-                        <td><?= htmlspecialchars($offre->pays) ?></td>
-                        <td><?= htmlspecialchars($offre->ville) ?></td>
-                        <td><?= htmlspecialchars($offre->prix) ?> €</td>
-                        <td><?= htmlspecialchars($offre->duree) ?> jours</td>
+                    <td class="text-center align-middle"><?= htmlspecialchars($offre->pays) ?></td>
+                    <td class="text-center align-middle"><?= htmlspecialchars($offre->ville) ?></td>
+                    <td class="text-center align-middle"><?= htmlspecialchars($offre->prix) ?> €</td>
+                    <td class="text-center align-middle"><?= htmlspecialchars($offre->duree) ?> jours</td>
                         <td class="text-wrap text-break w-25"> <?= htmlspecialchars($offre->description) ?></td>
-                        <td>
+                        <td class="text-center align-middle">
                             <?php if (!empty($offre->img)): ?>
                                 <img src="/Public/assets/images/<?= htmlspecialchars($offre->img) ?>" width="100" height="80" class="rounded shadow-sm">
                             <?php else: ?>
                                 <span class="text-muted">Aucune image</span>
                             <?php endif; ?>
                         </td>
-                        <td>
+                        <td class="text-center align-middle">
                             <a href="/DashDerniereMinute/updateDerniereMinute/<?= $offre->id ?>" class="btn btn-warning btn-sm">Modifier</a>
                             <form action="/DashDerniereMinute/deleteDerniereMinute" method="POST" class="d-inline" onsubmit="return confirm('Confirmer la suppression ?');">
                                 <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
@@ -42,5 +42,9 @@
                 <?php endforeach; ?>
             </tbody>
         </table>
+                <!-- BUTTON RETURN -->
+                <div class="text-end mb-4">
+        <a href="/Dashboard" class="btn btn-secondary">Retour</a>
+    </div>
     </div>
 </div>
