f8a46cd7e639c98b0346656f22a8b4ed8ee39968
Melissa Ould Youcef <ouldyoucefmelissa@gmail.com>
2025-04-02 22:05:43 +0200

    optimisation du doublon avec dashuser laisse juste le function confirm

--- Controllers/RegisterController.php
+++ Controllers/RegisterController.php
@@ -3,28 +3,9 @@
 namespace App\Controllers;
 
 use App\Services\ConfirmService;
-use App\Services\RegisterService;
 
 class RegisterController extends Controller
 {
-    public function index()
-    {
-        $this->render('register/index');
-    }
-
-    public function register()
-    {
-        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-            http_response_code(405);
-            echo json_encode(["status" => "error", "message" => "Méthode de requête non autorisée."]);
-            exit();
-        } else {
-            $data = $_POST;
-            $registerService = new RegisterService();
-            $registerService->register($data);
-        }
-    }
-
     public function confirm($token)
     {
         if ($token) {
